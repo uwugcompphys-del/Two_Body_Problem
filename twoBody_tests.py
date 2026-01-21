16148a993e7d6c7c19705c1ace10b7d6add33f88
Lachlan1005 <lachlankan5@gmail.com>
2026-01-21 10:36:34 -0500

    Modified docstring

--- twoBody_tests.py
+++ twoBody_tests.py
@@ -15,10 +15,27 @@ def generate_circular_orbits(r0:float, m:float=1, M:float=1):
     return mass1_initConds, mass2_initConds
 
 
-def verify_circular_orbits(mass1_positions:np.ndarray, mass2_positions:np.ndarray):
+def verify_circular_orbits(mass1_positions:np.ndarray, mass2_positions:np.ndarray, tol:float=1e-3) -> bool:
+    """
+    Returns whether or not the provided trajectories for mass 1 (mass1_positions) and mass 2(mass2_positions)are sufficiently circular according to the 
+    specified tolerance
+    Each trajectory is provided in the following format
+    mass_position = np.array([ [x0, y0], [x1, y1], [x2, y2],......., [x_final, y_final]])
+    Where (x_n, y_n) is the position coordinates of the mass at time t = t_n
+    
+    :param mass1_positions: Trajectory of mass1, format: np.array([ [x0, y0], [x1, y1], [x2, y2],......., [x_final, y_final]])
+    :type mass1_positions: np.ndarray
+
+    :param mass2_positions: Trajectory of mass2, format: np.array([ [x0, y0], [x1, y1], [x2, y2],......., [x_final, y_final]])
+    :type mass2_positions: np.ndarray
+
+    :param tol: Tolerance parameter for circular trajectory
+    :type tol: float
+    """
     separation = np.linalg.norm(mass1_positions[0]-mass2_positions[0])
     for i in range(len(mass1_positions)):
-        if np.abs(np.linalg.norm(mass1_positions[i]-mass2_positions[i])-separation)>1e-3:
+        if np.abs(np.linalg.norm(mass1_positions[i]-mass2_positions[i])-separation)>tol:
             print("The orbit is not circular!")
-            return None 
+            return False 
     print("The orbit is sufficiently circular")
+    return True
